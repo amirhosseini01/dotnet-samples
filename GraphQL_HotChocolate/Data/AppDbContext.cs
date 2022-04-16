4d3d2647ef0e9dec703ea21fee117115d5360925
amircsharp7 <amir.1derful@gmail.com>
2022-04-16 21:24:33 +0430

    Create AppDbContext File

---
+++ GraphQL_HotChocolate/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using GraphQL_HotChocolate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQL_HotChocolate.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Platform> Platforms { get; set; }
+}
