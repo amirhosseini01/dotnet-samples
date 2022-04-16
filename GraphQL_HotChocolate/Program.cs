0f73410829e5e557fb1288af62b170625d5bae98
amircsharp7 <amir.1derful@gmail.com>
2022-04-16 21:29:46 +0430

    Add Connection Str Configs to Program.cs

--- GraphQL_HotChocolate/Program.cs
+++ GraphQL_HotChocolate/Program.cs
@@ -1,4 +1,12 @@
+using GraphQL_HotChocolate.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(
+    builder.Configuration.GetConnectionString("CommandConStr")
+));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
