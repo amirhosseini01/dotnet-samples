3cbeb65463a29f492a3da46d01c8011e78075faa
amircsharp7 <amir.1derful@gmail.com>
2022-04-17 21:33:53 +0430

    GraphQL PipeLine and Program Cofigs

--- GraphQL_HotChocolate/Program.cs
+++ GraphQL_HotChocolate/Program.cs
@@ -1,4 +1,5 @@
 using GraphQL_HotChocolate.Data;
+using GraphQL_HotChocolate.GraphQL;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,8 +8,14 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(
     builder.Configuration.GetConnectionString("CommandConStr")
 ));
 
+builder.Services.AddGraphQLServer()
+.AddQueryType<Query>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseEndpoints(endpoints =>
+    endpoints.MapGraphQL()
+);
 
 app.Run();
