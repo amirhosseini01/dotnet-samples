d6cf6d53b7972858d59402e259dd91121bc38b28
amircsharp7 <amir.1derful@gmail.com>
2022-04-18 22:33:06 +0430

    Add GraphQl Voyager Document Description

--- GraphQL_HotChocolate/Models/Platform.cs
+++ GraphQL_HotChocolate/Models/Platform.cs
@@ -1,12 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace GraphQL_HotChocolate.Models;
+    [GraphQLDescription("Represent any software that have commandLine")]
     public class Platform
     {
         [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [GraphQLDescription("The Licence of the software")]
         public string LicenseKey { get; set; }
         public virtual ICollection<Command> Commands { get; set; }
     }
