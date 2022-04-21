1d5173e27b9f53a42349984d1efb62dbe5196b51
amircsharp7 <amir.1derful@gmail.com>
2022-04-21 23:11:17 +0430

    Complete AddPlatformAsync Method in GraphQL Mutation Class

--- GraphQL_HotChocolate/GraphQL/Mutation.cs
+++ GraphQL_HotChocolate/GraphQL/Mutation.cs
@@ -1,12 +1,21 @@
 using GraphQL_HotChocolate.Data;
 using GraphQL_HotChocolate.GraphQL.Platforms;
+using GraphQL_HotChocolate.Models;
 
 namespace GraphQL_HotChocolate.GraphQL;
 public class Mutation
 {
     [UseDbContext(typeof(AppDbContext))]
-   public async Task<AddPlatformPayload> AddPlatformAsync(AddPlatformInput input, [ScopedService] AppDbContext context)
-   {
-       return null;
-   }
+    public async Task<AddPlatformPayload> AddPlatformAsync(AddPlatformInput input, [ScopedService] AppDbContext context)
+    {
+        var platform = new Platform()
+        {
+            Name = input.Name
+        };
+
+        await context.Platforms.AddAsync(platform);
+        await context.SaveChangesAsync();
+
+        return new AddPlatformPayload(platform);
+    }
 }
