75059625643a9b68fbfc0eb1ab4b7a919c67e3a6
amircsharp7 <amir.1derful@gmail.com>
2022-04-29 13:22:10 +0430

    LaunchDto Class

---
+++ BlazorAPIClient/Dtos/LaunchDto.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+namespace BlazorAPIClient.Dtos;
+
+public class LaunchDto
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("is_tentative")]
+    public bool IsTentative { get; set; }
+
+    [JsonPropertyName("launch_date_local")]
+    public DateTimeOffset LaunchDateLocal { get; set; }
+
+    [JsonPropertyName("mission_name")]
+    public string MissionName { get; set; }
+}
