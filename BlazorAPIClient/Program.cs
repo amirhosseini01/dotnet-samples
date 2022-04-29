6af296149e49c346cb7f1d7579a11d4c3d80d37c
amircsharp7 <amir.1derful@gmail.com>
2022-04-29 17:11:21 +0430

    Inject ISpaceXDataService

--- BlazorAPIClient/Program.cs
+++ BlazorAPIClient/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorAPIClient;
+using BlazorAPIClient.DataService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<ISpaceXDataService, RestSpaceXDataService>();
 builder.Services.AddScoped<HttpClient>();
 
 await builder.Build().RunAsync();
