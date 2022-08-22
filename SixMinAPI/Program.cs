83d5d3c27e1f8e1942ce204e641302615f9bc34b
amircsharp7 <amir.1derful@gmail.com>
2022-08-22 23:17:05 +0430

    First minimal endpoint

--- SixMinAPI/Program.cs
+++ SixMinAPI/Program.cs
@@ -1,6 +1,8 @@
+using AutoMapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SixMinAPI.Data;
+using SixMinAPI.Models;
 using SixMinAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,10 +12,20 @@ builder.Services.AddSwaggerGen();
 
 // sql server configuration
 var sqlServerBuilder = new SqlConnectionStringBuilder();
-sqlServerBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlServerBuilder.ConnectionString = builder.Configuration
+.GetConnectionString("SQLDbConnection");
 sqlServerBuilder.UserID = builder.Configuration["UserId"];
 sqlServerBuilder.Password = builder.Configuration["Password"];
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlServerBuilder.ConnectionString));
+builder.Services.AddDbContext<AppDbContext>(opt =>
+opt.UseSqlServer(sqlServerBuilder.ConnectionString));
+
+builder.Services.AddCors(o => o.AddPolicy("AllowAnyOrigin",
+                      builder =>
+                       {
+                           builder.AllowAnyOrigin()
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader();
+                       }));
 
 // DI services
 builder.Services.AddScoped<CommandRepo>();
@@ -30,5 +42,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAnyOrigin");
+
+app.MapGet("api/v1/commands", async (CommandRepo repo, IMapper mapper) =>
+{
+    var commands = await repo.GetAsync();
+    return Results.Ok(mapper.Map<IList<Command>>(commands));
+});
 
 app.Run();
