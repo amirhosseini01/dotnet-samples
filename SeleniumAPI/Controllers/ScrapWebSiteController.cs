a391676dd0fdc3a719f1492c252e2d11d8f2d5fb
amircsharp7 <amir.1derful@gmail.com>
2022-10-06 18:01:52 +0330

    Getting image url using webdriver

--- SeleniumAPI/Controllers/ScrapWebSiteController.cs
+++ SeleniumAPI/Controllers/ScrapWebSiteController.cs
@@ -1,5 +1,9 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumAPI.Controllers;
 
@@ -8,12 +12,37 @@ namespace SeleniumAPI.Controllers;
 public class ScrapWebSiteController : ControllerBase
 {
     [HttpGet]
-    public async Task Get()
+    public long Get()
     {
-        var driver = new ChromeDriver();
+        Stopwatch sw = new();
+        sw.Start();
 
-        driver.Navigate().GoToUrl("https://selenium.dev");
+        using var driver = new ChromeDriver(chromeDriverDirectory:
+            "C:\\Program Files\\Google\\Chrome\\Application");
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        driver.Url = "https://www.digikala.com/";
 
-        driver.Quit();
+        IReadOnlyCollection<IWebElement> specialProducts =
+            driver.FindElements(
+                By.CssSelector(".d-block.pointer.pos-relative.bg-000.overflow-hidden.grow-1.py-3.px-4.px-2-lg.h-full-md.shrink-0.ml-1.ml-0-lg.px-3-xs.px-4-lg"))
+            ;
+        if (specialProducts is null || specialProducts.Count == 0)
+            return -1;
+
+        int deltaY = specialProducts!.First().Location.Y;
+        new Actions(driver)
+            .ScrollByAmount(0, deltaY)
+            .Perform();
+
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+        foreach (var item in specialProducts)
+        {
+            var test = item.FindElement(By.TagName("img")).GetAttribute("src");
+        }
+
+        sw.Stop();
+        driver.Close();
+        return sw.ElapsedMilliseconds;
     }
 }
