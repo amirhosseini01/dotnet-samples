7b6dbf571efa44cb90b36abefcaca4ac72ab321a
amircsharp7 <amir.1derful@gmail.com>
2022-10-14 13:47:46 +0330

    Other way of waits

--- SeleniumAPI/Controllers/ScrapWebSiteController.cs
+++ SeleniumAPI/Controllers/ScrapWebSiteController.cs
@@ -11,8 +11,9 @@ namespace SeleniumAPI.Controllers;
 [Route("[controller]")]
 public class ScrapWebSiteController : ControllerBase
 {
-    [HttpGet]
-    public long Get()
+    private const string _specialProductsCssSelector = ".d-block.pointer.pos-relative.bg-000.overflow-hidden.grow-1.py-3.px-4.px-2-lg.h-full-md.shrink-0.ml-1.ml-0-lg.px-3-xs.px-4-lg";
+    [HttpGet(nameof(GetImplicitWait))]
+    public long GetImplicitWait()
     {
         Stopwatch sw = new();
         sw.Start();
@@ -24,8 +25,44 @@ public class ScrapWebSiteController : ControllerBase
 
         IReadOnlyCollection<IWebElement> specialProducts =
             driver.FindElements(
-                By.CssSelector(".d-block.pointer.pos-relative.bg-000.overflow-hidden.grow-1.py-3.px-4.px-2-lg.h-full-md.shrink-0.ml-1.ml-0-lg.px-3-xs.px-4-lg"))
+                By.CssSelector(_specialProductsCssSelector))
             ;
+        if (specialProducts is null || specialProducts.Count == 0)
+            return -1;
+
+        int deltaY = specialProducts!.First().Location.Y;
+        new Actions(driver)
+            .ScrollByAmount(0, deltaY)
+            .Perform();
+
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+        foreach (var item in specialProducts)
+        {
+            var test = item.FindElement(By.TagName("img")).GetAttribute("src");
+        }
+
+        sw.Stop();
+        driver.Close();
+        return sw.ElapsedMilliseconds;
+    }
+    [HttpGet(nameof(GetExplicitWait))]
+    public long GetExplicitWait()
+    {
+        Stopwatch sw = new();
+        sw.Start();
+
+        using var driver = new ChromeDriver(chromeDriverDirectory:
+            "C:\\Program Files\\Google\\Chrome\\Application");
+        driver.Url = "https://www.digikala.com/";
+
+        WebDriverWait wait = new(driver, TimeSpan.FromSeconds(10))
+        {
+            PollingInterval = TimeSpan.FromSeconds(10),
+        };
+          wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+        IReadOnlyCollection<IWebElement> specialProducts = wait.Until(e => e.FindElements(By.CssSelector(_specialProductsCssSelector)));
         if (specialProducts is null || specialProducts.Count == 0)
             return -1;
 
