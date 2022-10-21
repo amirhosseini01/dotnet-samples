004f03d67d025de7b492a90c3667a2c2bff46416
amircsharp7 <amir.1derful@gmail.com>
2022-10-21 15:57:24 +0330

    simple adding data

--- BlazorOfflineDB/Program.cs
+++ BlazorOfflineDB/Program.cs
@@ -1,11 +1,27 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorOfflineDB;
+using BlazorDB;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorDB(options =>
+{
+    options.Name = "Test";
+    options.Version = 1;
+    options.StoreSchemas = new List<StoreSchema>()
+    {
+        new StoreSchema()
+        {
+            Name = "Person",
+            PrimaryKey = "id",
+            PrimaryKeyAuto = true,
+            UniqueIndexes = new List<string> { "name" }
+        }
+    };
+});
 
 await builder.Build().RunAsync();
