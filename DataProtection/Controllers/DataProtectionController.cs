bb4f91f5fe19ee424ad9a992d5ba2cd1f9d32abd
amir <amir.1derful@gmail.com>
2023-02-03 20:43:36 +0330

    hash password using dataProtection

--- DataProtection/Controllers/DataProtectionController.cs
+++ DataProtection/Controllers/DataProtectionController.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 
@@ -55,5 +57,23 @@ public class DataProtectionController : ControllerBase
     {
         var myProtector = GetProtector(provider: protectionProvider, purpose: purpose);
         return myProtector.Protect(unprotectedText);
+    }
+
+    [HttpGet("HashPassword/{password}")]
+    public string HashPassword(string password)
+    {
+        // Generate a 128-bit salt using a sequence of
+        // cryptographically strong random bytes.
+        byte[] salt = RandomNumberGenerator.GetBytes(128 / 8); // divide by 8 to convert bits to bytes
+
+        // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        return hashed;
     }
 }
